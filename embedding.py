c3073e8e9ba9cecf67489c3cc5c7eb10a4550c68
XoriieInpottn <XoriieInpottn@126.com>
2026-01-27 17:27:04 +0800

    experimental

---
+++ embedding.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+__author__ = "xi"
+
+import httpx
+import numpy as np
+from agent_types.common import LLMConfig
+from agent_types.common import NDArray
+from agent_types.retrieval import DenseEmbeddingRequest
+from agent_types.retrieval import DenseEmbeddingResponse
+from libentry.mcp.client import APIClient
+from openai import OpenAI
+
+
+class EmbeddingAdapter:
+
+    def __init__(self, url: str | LLMConfig):
+        self.url = url
+
+    def embedding(self, request: DenseEmbeddingRequest) -> DenseEmbeddingResponse:
+        if isinstance(self.url, str):
+            with APIClient(self.url) as client:
+                return DenseEmbeddingResponse.model_validate(client.post(request))
+        else:
+            with OpenAI(
+                    base_url=self.url.base_url,
+                    api_key=self.url.api_key,
+                    http_client=httpx.Client(verify=False)
+            ) as client:
+                response = client.embeddings.create(
+                    model=self.url.model,
+                    input=request.text,
+                )
+
+            data = response.data
+            data.sort(key=lambda _i: _i.index)
+            data = [
+                [float(val) for val in row.embedding]
+                for row in data
+            ]
+            if isinstance(request.text, str):
+                data = data[0]
+
+            emb = np.array(data, dtype=np.float32)
+            if request.normalize:
+                emb /= (np.linalg.norm(emb, 2, -1, keepdims=True) + 1e-30)
+            emb = NDArray.from_array(emb)
+            return DenseEmbeddingResponse(trace_id=request.trace_id, embedding=emb)
--- main.py
+++ main.py
@@ -2,19 +2,31 @@
 
 __author__ = "xi"
 
-from elasticsearch import Elasticsearch
+from agent_types.common import LLMConfig
+from agent_types.retrieval import DenseEmbeddingRequest
+from elasticsearch import Elasticsearch, helpers
+from pydantic import BaseModel
+
+from embedding import EmbeddingAdapter
 
 TEMPLATE_NAME = "objects_template"
 INDEX_NAME = "objects_index"
 
 
-class Mem:
+class RawMemory:
+    """Raw memory module."""
 
-    def __init__(self):
-        self.es = Elasticsearch(
-            "http://localhost:9200",
-            basic_auth=("elastic", "tangxx11")
-        )
+    class Config(BaseModel):
+        embedding_url: str | LLMConfig
+        embedding_dims: int
+        index_name: str = INDEX_NAME
+        template_name: str = TEMPLATE_NAME
+
+    def __init__(self, config: Config):
+        self.config = config
+
+        self.embedding_adapter = EmbeddingAdapter(self.config.embedding_url)
+        self.es = Elasticsearch("http://localhost:9200", basic_auth=("elastic", "tangxx11"))
 
     def get_index_template(self, name: str = TEMPLATE_NAME):
         for t in self.es.indices.get_index_template()["index_templates"]:
@@ -29,12 +41,12 @@ class Mem:
         return None
 
     def destroy(self):
-        if self.es.indices.exists(index=INDEX_NAME):
-            self.es.indices.delete(index=INDEX_NAME)
-        if self.es.indices.exists_index_template(name=TEMPLATE_NAME):
-            self.es.indices.delete_index_template(name=TEMPLATE_NAME)
+        if self.es.indices.exists(index=self.config.index_name):
+            self.es.indices.delete(index=self.config.index_name)
+        if self.es.indices.exists_index_template(name=self.config.template_name):
+            self.es.indices.delete_index_template(name=self.config.template_name)
 
-    def init_index(self, vector_dims: int):
+    def initialize(self):
         if self.get_index_template():
             raise RuntimeError("Already initialized.")
 
@@ -86,20 +98,19 @@ class Mem:
             "text": {"type": "text", "analyzer": "standard"},
             "vector": {
                 "type": "dense_vector",
-                "dims": vector_dims,
+                "dims": self.config.embedding_dims,
                 "index": True,
                 "similarity": "cosine"
-            },
-            "payload": {"type": "object", "enabled": False}
+            }
         }
         template_body = {
-            "index_patterns": [INDEX_NAME],
+            "index_patterns": [self.config.index_name],
             "priority": 100,
             "template": {
-                "settings": {
-                    "number_of_shards": 1,
-                    "number_of_replicas": 0
-                },
+                # "settings": {
+                #     "number_of_shards": 1,
+                #     "number_of_replicas": 0
+                # },
                 "mappings": {
                     "dynamic": True,
                     "dynamic_templates": dynamic_templates,
@@ -116,14 +127,73 @@ class Mem:
             body=template_body
         )
 
-        self.es.indices.create(index=INDEX_NAME)
+        self.es.indices.create(index=self.config.index_name)
+
+    def insert(self, docs: list[dict] | dict):
+        if isinstance(docs, dict):
+            docs = [docs]
+
+        text = [doc["text"] for doc in docs]
+        response = self.embedding_adapter.embedding(DenseEmbeddingRequest(text=text, normalize=False))
+        for doc, vector in zip(docs, response.embedding.to_array().tolist()):
+            doc["vector"] = vector
+
+        helpers.bulk(self.es, [
+            {"_index": self.config.index_name, "_source": doc}
+            for doc in docs
+        ])
+
+    def search(self, query: str, k=5, num_candidates=50):
+        emb = self.embedding_adapter.embedding(DenseEmbeddingRequest(text=query, normalize=False)).embedding
+        query_vector = emb.to_array().tolist()
+
+        resp = self.es.search(
+            index=self.config.index_name,
+            query={
+                "bool": {
+                    "must": [
+                        {"match": {"text": query}}
+                    ]
+                }
+            },
+            knn={
+                "field": "vector",
+                "query_vector": query_vector,
+                "k": k,
+                "num_candidates": num_candidates
+            }
+        )
+        docs = []
+        for hit in resp["hits"]["hits"]:
+            docs.append({k: v for k, v in hit["_source"].items() if k != "vector"})
+            print(hit["_id"], hit["_score"], docs[-1])
 
 
 def main():
-    mem = Mem()
-    mem.destroy()
-    mem.init_index(1024)
-    print(mem.get_index_info())
+    data = [
+        "Objects index template with keyword-first dynamic mapping",
+        "什么是索引模板，什么是动态映射",
+        "How to retrieval from a database",
+        "今天太冷了",
+        "动态索引模板可以用来定义索引的默认类型"
+    ]
+    service = RawMemory(RawMemory.Config(
+        embedding_url=LLMConfig(
+            base_url="https://api.zhizengzeng.com/v1",
+            api_key="zk-08507a246a7f1fc844185def5ae9ef2b",
+            model="text-embedding-3-small"
+        ),
+        embedding_dims=1536
+    ))
+
+    # service.destroy()
+    # service.initialize()
+    # service.insert([
+    #     {"text": item, "type": "knowledge"}
+    #     for item in data
+    # ])
+
+    service.search("dynamic template")
     return 0
 
 
